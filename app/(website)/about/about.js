dbfbe9c9698221c60b3dab808b3c714ca9dff47f
Thom Crowe <thom@thomcrowe.com>
2023-09-24 23:06:30 -0500

    updating for SEO

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -25,7 +25,7 @@ export default function About({ authors, settings }) {
 
       <div className="mx-auto prose text-left dark:prose-invert mt-14">
          <p>
-            Anglo-Orthodoxy shares a spiritual history with the Anglo-Catholic tradition. The Anglo-Catholic tradition traces its roots back to the 18th Century Oxford Movement and the Tractarians who argued that Anglicanism was truly a part of the Catholic tradition of the Christian Church and carried the flame of Apostolic Succession. Anglo-Catholoism brought a return to elements of the liturgy that were lost in the BCP, prioritized weekly communion, returned altar candles, brought back incense, just to name a few contributions.
+            Those in the Anglo-Orthodox tradition, or Anglo-Orthodoxy, share a spiritual history with the Anglo-Catholic tradition. The Anglo-Catholic tradition traces its roots back to the 18th Century Oxford Movement and the Tractarians who argued that Anglicanism was truly a part of the Catholic tradition of the Christian Church and carried the flame of Apostolic Succession. Anglo-Catholoism brought a return to elements of the liturgy that were lost in the BCP, prioritized weekly communion, returned altar candles, brought back incense, just to name a few contributions.
           </p>
           <p>
             Those of us who identify as Anglo-Orthodox have an affinity for early Christian practicies and the expressions of worship held in the Christian East. We have icons, use icons to pray, prioritize confession, cross ourselves more freqenctly, omit the Filioque from the Creed, pray the Jesus Prayer with prayer ropes, just to name a few practices we incorporate into our daily lives. 
@@ -33,7 +33,7 @@ export default function About({ authors, settings }) {
           <p>            
             We see the Anglican Tradition as a beautiful tradition that offers space <i>via media</i> , allowing those of us who are drawn to and feel comfortable with expressions of worship typically associated with the East. 
           </p>
-          <font size="+1">So why Anglo-Orthodoxy?</font> 
+          <font size="+1">So why Anglo-Orthodox?</font> 
           <p>There are many, many treasures from the Christian East that are not well known here in the West. Anglo-Orthodoxy looks so bring that light and share it here within our Anglican Tradition, much as Archbishop Rowan Williams did with his book <a href="https://www.amazon.com/Looking-East-Winter-Contemporary-Christian/dp/1472989244">Looking East in Winter: Contemporary Thought and the Eastern Christian Tradition</a> - though, admittedly not on par with the work of the Archbishop.  We hope you will enjoy insights from the writings of the East, explorations of practices, and thoughts on an Eastern worldview.</p>
         <p>
           <Link href="/contact">Get in touch</Link>
